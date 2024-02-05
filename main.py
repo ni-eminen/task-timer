4ab499e60408ba7651e7a692ff8ec44154f2be60
ni-eminen <matiasnie2@gmail.com>
2024-02-05 20:09:56 +0200

    init

---
+++ main.py
@@ -0,0 +1,36 @@
+import time
+import os
+import threading
+from pynput import keyboard
+cmd = input('Topic: ')
+global coffee_count
+coffee_count = 0
+
+def timer():
+    global coffee_count
+    elapsed = 0
+    while(True):
+        time.sleep(0.1)
+        elapsed += .1
+        os.system('clear')
+        print(f'Topic: {cmd}')
+        print(f'Elapsed time: {int(elapsed // 60)} min, {round(elapsed % 60, 2)} sec')
+        print(f'coffees today: {coffee_count}')
+
+def on_press(key):
+        try:
+            if key.char == 'c':
+                global coffee_count
+                coffee_count += 1
+            print('alphanumeric key {0} pressed'.format(
+                key.char))
+        except AttributeError:
+            # Special key
+            pass
+
+# ...or, in a non-blocking fashion:
+listener = keyboard.Listener(
+    on_press=on_press)
+listener.start()
+
+timer()
